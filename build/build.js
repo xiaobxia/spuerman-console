c4e67d4bbf0849701a971af570fdf556f9f7d1b2
chenlingjie <chenlingjie@cd121.com>
2017-09-07 15:27:09 +0800

    增加build说明

--- build/build.js
+++ build/build.js
@@ -12,7 +12,7 @@ var webpackConfig = require('./webpack.prod.conf')
 
 var spinner = ora('building for production...')
 spinner.start()
-
+//TODO 只适合单页应用，因为它不删html，只删static
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
   webpack(webpackConfig, function (err, stats) {
