2be28f1d7a81332099b92e799593696dcc55bd0a
chenlingjie <chenlingjie@cd121.com>
2017-11-30 17:03:31 +0800

    xg

--- build/build.js
+++ build/build.js
@@ -10,10 +10,24 @@ var webpack = require('webpack')
 var config = require('../config')
 var webpackConfig = require('./webpack.prod.conf')
 const fs = require('fs')
-const upload = require('./cdn');
+const cdn = require('./cdn');
+const baseDir = path.resolve(__dirname, '../dist');
 
 var spinner = ora('building for production...')
 spinner.start()
+
+cdn.getCdnFileList().then((fileList) => {
+  if (!fileList.length) {
+    console.log('no file');
+    return;
+  }
+  cdn.deleteCdnFile(fileList.map((item) => {
+    return item.key;
+  })).then(() => {
+    console.log('all delete')
+  });
+});
+
 //TODO 只适合单页应用，因为它不删html，只删static
 rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
   if (err) throw err
@@ -21,12 +35,12 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
     spinner.stop()
     if (err) throw err
     process.stdout.write(stats.toString({
-      colors: true,
-      modules: false,
-      children: false,
-      chunks: false,
-      chunkModules: false
-    }) + '\n\n')
+        colors: true,
+        modules: false,
+        children: false,
+        chunks: false,
+        chunkModules: false
+      }) + '\n\n')
 
     console.log(chalk.cyan('  Build complete.\n'))
     console.log(chalk.yellow(
@@ -34,9 +48,9 @@ rm(path.join(config.build.assetsRoot, config.build.assetsSubDirectory), err => {
       '  Opening index.html over file:// won\'t work.\n'
     ))
 
-    let baseDir = path.resolve(__dirname, '../dist');
+    //上传cdn
     const promiseList = fs.readdirSync(baseDir).map(function (file) {
-      return upload(path.resolve(baseDir, file), file);
+      return cdn.upload(path.resolve(baseDir, file), file);
     });
     Promise.all(promiseList).then(function () {
       console.log(chalk.cyan('  upload complete.\n'))
